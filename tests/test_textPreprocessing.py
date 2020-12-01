405d77a3be79b3619762efa88399f6079c003188
stephens <giovanni.stephens@trustpower.co.nz>
2020-12-02 12:22:21 +1300

    Added a test to see whether the default pipeline would load fine when the class is instantiated.

--- tests/test_textPreprocessing.py
+++ tests/test_textPreprocessing.py
@@ -15,9 +15,15 @@ class test_textPreprocessing(unittest.TestCase):
             tp.TextPreprocessing(self.test_utterances)
 
     def test_pipes_loaded(self):
-        """Tests that the NLP pipeline has been correctly set up."""
-        self.assertEquals(self.proprocessor.nlp.pipe_names, \
+        """Tests that the SpaCy NLP pipeline has been correctly set up."""
+        self.assertEqual(self.proprocessor.nlp.pipe_names, \
              ['tagger', 'sentencizer', 'parser', 'ner', 'entity_ruler'])
+
+    def test_no_additonal_pipes(self):
+        """Tests that the default pipes get loaded in the SpaCy NLP pipeline"""
+        preprocessor = tp.TextPreprocessing(self.test_utterances, pipes=None)
+        self.assertEqual(preprocessor.nlp.pipe_names, \
+             ['tagger', 'parser', 'ner'])
 
 if __name__ == '__main__':
     unittest.main()
